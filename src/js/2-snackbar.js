0fa8fa4dff8de662b13260b87a486741632639ac
Tatiana88888 <147710787+Tatiana88888@users.noreply.github.com>
2024-03-18 18:44:12 +0200

    add all js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,34 +1,33 @@
-import iziToast from "izitoast"
+import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-
 const formEl = document.querySelector(".form");
-
-formEl.addEventListener("submit",handleSubmit);
-
-function handleSubmit (event) {
-    event.preventDefault();
-    const delay = event.currentTarget.elements.delay.value;
-    const state = event.currentTarget.elements.state.value;
-   
- const promise = new Promise((resolve,reject) => setTimeout(() => {if(state === "fulfilled"){
-   resolve (delay)} else{reject(delay)}},delay))
-    return promise;
+formEl.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const delay = event.currentTarget.elements.delay.value;
+  const state = event.currentTarget.elements.state.value;
+  const promise = new Promise((resolve, reject) => setTimeout(() => {
+    if (state === "fulfilled") {
+      resolve(delay);
+    } else {
+      reject(delay);
+    }
+  }, delay));
+  promise
+    .then(value => {
+      iziToast.success({
+        color: 'green',
+        position: 'topRight',
+        message: `Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: `Rejected promise in ${delay}ms`,
+      });
+    });
 }
-
-promise
-.then(value => {
-  iziToast.success({
-    color: 'green',
-    position: 'topRight',
-    message: `✅ Fulfilled promise in ${delay}ms`,
-  });
-})
-.catch(error => {
-  iziToast.error({
-    color: 'red',
-    position: 'topRight',
-    message: `❌ Rejected promise in ${delay}ms`,
-  });
-});
 
 
